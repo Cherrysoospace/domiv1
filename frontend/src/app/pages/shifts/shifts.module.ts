df31a44de450ad78661626ed5b5683a93fe7916d
Cherrysoospace <thinkandbreathmate@gmail.com>
2025-11-15 21:56:21 -0500

    Creación de modulos principales

---
+++ frontend/src/app/pages/shifts/shifts.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShiftsRoutingModule } from './shifts-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ShiftsRoutingModule
+  ]
+})
+export class ShiftsModule { }
